8290677e632149ea494655bbe0ebc6722ef834ea
SergeyKraishkin <103445301+SergeyKraishkin@users.noreply.github.com>
2023-11-20 18:25:35 +0300

    Add files via upload
    Первая редакция 20 ноября 2023

---
+++ public/homePage.js
@@ -0,0 +1,112 @@
+"use strict";
+
+const Logout = new LogoutButton();
+Logout.action = function act() {
+    ApiConnector.logout(callback => {
+        if (callback.success === true) {
+            location.reload();
+        } else {
+            userForm.setLoginErrorMessage("Не удалось выйти");
+        }
+    });
+};
+
+ApiConnector.current(callback => {
+    if (callback.success === true)
+        ProfileWidget.showProfile(callback.data);
+
+});
+
+/* личный код получения курса валют (для памяти)
+let xhr = new XMLHttpRequest();
+
+function getcourse() {
+    let courses = xhr.response;
+    return (courses.Valute.USD.Value);
+}
+xhr.onload = getcourse;
+xhr.open("GET", "https://www.cbr-xml-daily.ru/daily_json.js");
+xhr.responseType = 'json';
+xhr.send();
+*/
+const Rates = new RatesBoard();
+getcourse();
+const intervalID = setInterval(getcourse, 60000);
+
+function getcourse() {
+    ApiConnector.getStocks(callback => {
+        if (callback.success === true) {
+            Rates.clearTable();
+            Rates.fillTable(callback.data);
+        }
+    });
+}
+
+const moneyManager = new MoneyManager();
+const favoritesWidget = new FavoritesWidget();
+moneyManager.addMoneyCallback = function addMoney(...args) {
+    ApiConnector.addMoney(...args, callback => {
+        if (callback.success === true) {
+            ProfileWidget.showProfile(callback.data);
+            favoritesWidget.setMessage(true, "Транзакция прошла успешно");
+        } else {
+            favoritesWidget.setMessage(false, "Транзакция не прошла");
+        }
+    });
+}
+moneyManager.conversionMoneyCallback = function convertMoney(...args) {
+    ApiConnector.convertMoney(...args, callback => {
+        if (callback.success === true) {
+            ProfileWidget.showProfile(callback.data);
+            favoritesWidget.setMessage(true, "Транзакция прошла успешно");
+        } else {
+            favoritesWidget.setMessage(false, "Транзакция не прошла");
+        }
+    });
+}
+
+moneyManager.sendMoneyCallback = function sendMoney(...args) {
+    ApiConnector.transferMoney(...args, callback => {
+        if (callback.success === true) {
+            ProfileWidget.showProfile(callback.data);
+            favoritesWidget.setMessage(true, "Транзакция прошла успешно");
+        } else {
+            favoritesWidget.setMessage(false, "Транзакция не прошла");
+        }
+    });
+}
+
+ApiConnector.getFavorites(callback => {
+    if (callback.success === true) {
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(callback.data);
+        moneyManager.updateUsersList(callback.data);
+    } else {
+        favoritesWidget.setMessage(false, "Не получилось обновить");
+    }
+});
+favoritesWidget.addUserCallback = function addUserFav(...args) {
+    ApiConnector.addUserToFavorites(...args, callback => {
+        if (callback.success === true) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(callback.data);
+            moneyManager.updateUsersList(callback.data);
+            favoritesWidget.setMessage(true, "Пользователь добавлен в избранные");
+        } else {
+            favoritesWidget.setMessage(false, "Не получилось добавить пользователя в избранные");
+        }
+    });
+}
+
+favoritesWidget.removeUserCallback = function removeUserFav(...args) {
+    ApiConnector.removeUserFromFavorites(...args, callback => {
+        if (callback.success === true) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(callback.data);
+            moneyManager.updateUsersList(callback.data);
+            favoritesWidget.setMessage(true, "Пользователь удален из избранных");
+        } else {
+            favoritesWidget.setMessage(false, "Не получилось удалить пользователя из избранных");
+        }
+    });
+}
---
+++ public/loginPage.js
@@ -0,0 +1,23 @@
+ "use strict";
+
+
+ const userForm = new UserForm();
+ userForm.loginFormCallback = (...args) => {
+     ApiConnector.login(...args, callback => {
+         if (callback.success === true) {
+             location.reload();
+         } else {
+             userForm.setLoginErrorMessage("Не правильный логин или пароль");
+         }
+     });
+ }
+
+ userForm.registerFormCallback = (...args) => {
+     ApiConnector.register(...args, callback => {
+         if (callback.success === true) {
+             location.reload();
+         } else {
+             userForm.setLoginErrorMessage("Не удалось зарегистрировать пользователя");
+         }
+     });
+ }
